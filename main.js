050aaacd5de85e07ff7bb747ed04f60683e6de1d
benjflowers <benjaminjflowers@gmail.com>
2024-06-29 23:32:02 -0500

    basic setup

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 console.log('lets make something')
+// setting global things up here
+/**
+ * @type {Number}
+ */
+let width
+/**
+ * @type {Number}
+ */
+let height
+
+// default size is 300x150
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+const setupConfiguration = {
+  canvas: canvas,
+  width: 500,
+  height: 500
+}
+
+/**
+ * 
+ * @param {HTMLCanvasElement} canvas 
+ * @param {Number} width 
+ * @param {Number} height 
+ */
+const setCanvasSize = (canvas, width, height) => {
+  canvas.setAttribute('width', width)
+  canvas.setAttribute('height', height)
+}
+
+const setup = () => {
+  setCanvasSize(setupConfiguration.canvas, setupConfiguration.width, setupConfiguration.height)
+}
+
+setup()
